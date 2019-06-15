e9dd7e163638f5b95be377d6352d65aa68f54e54
dernasherbrezon <rodionovamp@mail.ru>
2019-06-15 20:33:06 +0100

    back to java 8 + add some documentation

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # rtlSpectrum
 Analyze spectrograms created by rtl_power
+
+# Screenshots
+
+![screen1](/screenshots/1.png?raw=true)
+![screen2](/screenshots/2.png?raw=true)
+
+# Features
+
+* load from .csv file produced by rtl_power
+* run rtl_power directly. it should be available in the $PATH
+
+# Installation
+
+* Ensure you have java installed. Required version is 1.8+
+* Go to [Releases](https://github.com/dernasherbrezon/rtlSpectrum/releases) tab
+* Download the latest .jar file
+* Run it using the command ```java -jar rtlSpectrum.jar```
--- src/main/java/ru/r2cloud/rtlspectrum/ReadFromFile.java
+++ src/main/java/ru/r2cloud/rtlspectrum/ReadFromFile.java
@@ -3,7 +3,6 @@ package ru.r2cloud.rtlspectrum;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -26,7 +25,7 @@ public class ReadFromFile extends StatusBarTask<List<XYChart.Data<Number, Number
 		long minimum = 24_000_000;
 		long maximum = 1_700_000_000;
 		ArrayList<XYChart.Data<Number, Number>> result = new ArrayList<>();
-		try (BufferedReader r = new BufferedReader(new FileReader(file, StandardCharsets.ISO_8859_1))) {
+		try (BufferedReader r = new BufferedReader(new FileReader(file))) {
 			String curLine = null;
 			while ((curLine = r.readLine()) != null) {
 				XYChart.Data<Number, Number> cur = parse(curLine);
