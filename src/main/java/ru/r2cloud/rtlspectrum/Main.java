978f92dcb83c1339f6049a3e6c5ed0eb13227b9f
dernasherbrezon <rodionovamp@mail.ru>
2020-03-19 14:26:26 +0000

    fix jdk11 bundle

--- src/main/java/ru/r2cloud/rtlspectrum/Main.java
+++ src/main/java/ru/r2cloud/rtlspectrum/Main.java
@@ -1,49 +1,9 @@
 package ru.r2cloud.rtlspectrum;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Locale;
+public class Main {
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-	private Controller controller;
-
-	@Override
-	public void start(Stage stage) {
-		Parent root;
-		try (InputStream is = getClass().getClassLoader().getResourceAsStream("layout.fxml")) {
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("layout.fxml"));
-			root = fxmlLoader.load(is);
-			controller = fxmlLoader.getController();
-		} catch (IOException e) {
-			throw new IllegalStateException(e);
-		}
-
-		Scene scene = new Scene(root, 640, 480);
-		String osName = System.getProperty("os.name");
-		if (osName != null && osName.toLowerCase(Locale.UK).contains("mac") && MacOsUtil.isDark("/usr/bin/defaults")) {
-			scene.getStylesheets().add("dark.css");
-		}
-		stage.setScene(scene);
-		stage.setTitle("rtlSpectrum");
-		stage.show();
-	}
-
-	@Override
-	public void stop() throws Exception {
-		if (controller != null) {
-			controller.stop();
-		}
-	}
-
-	public static void main(String[] args) {
-		launch();
+	public static void main(String[] args) throws Exception {
+		MainApplication.main(args);
 	}
 
 }
---
+++ src/main/java/ru/r2cloud/rtlspectrum/MainApplication.java
@@ -0,0 +1,49 @@
+package ru.r2cloud.rtlspectrum;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+	private Controller controller;
+
+	@Override
+	public void start(Stage stage) {
+		Parent root;
+		try (InputStream is = getClass().getClassLoader().getResourceAsStream("layout.fxml")) {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("layout.fxml"));
+			root = fxmlLoader.load(is);
+			controller = fxmlLoader.getController();
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+
+		Scene scene = new Scene(root, 640, 480);
+		String osName = System.getProperty("os.name");
+		if (osName != null && osName.toLowerCase(Locale.UK).contains("mac") && MacOsUtil.isDark("/usr/bin/defaults")) {
+			scene.getStylesheets().add("dark.css");
+		}
+		stage.setScene(scene);
+		stage.setTitle("rtlSpectrum");
+		stage.show();
+	}
+
+	@Override
+	public void stop() throws Exception {
+		if (controller != null) {
+			controller.stop();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
