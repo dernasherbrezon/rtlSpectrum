c512ed76f5aa949e4447a640e776d25dd0d6b860
dernasherbrezon <rodionovamp@mail.ru>
2020-07-17 08:44:29 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/rtlspectrum/Main.java
+++ src/main/java/ru/r2cloud/rtlspectrum/Main.java
@@ -2,7 +2,7 @@ package ru.r2cloud.rtlspectrum;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		MainApplication.main(args);
 	}
 
